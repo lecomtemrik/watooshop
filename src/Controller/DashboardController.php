a4c4d4e53956325535b0482a9884d2bccb571229
Emrik <lecomtemrik@gmail.com>
2022-06-11 23:28:58 +0200

    dahsboard => add/update asin

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\AsinFormType;
+use App\Service\AmazonApi;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
+    private ManagerRegistry $doctrine;
+    private AmazonApi $AmazonApi;
+
+    public function __construct(ManagerRegistry $doctrine, AmazonApi $amazonApi)
+    {
+        $this->doctrine = $doctrine;
+        $this->AmazonApi = $amazonApi;
+
+    }
+
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
@@ -15,4 +30,71 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
+    #[Route('/adminProduct', name: 'admin_product')]
+    public function addProduct(Request $request): Response
+    {
+        $form = $this->createForm(AsinFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $asin = $form->getData()['asin'];
+            if ($form->get('add')->isClicked()){
+                $this->scraperAmazonProduct($asin);
+            }
+            if ($form->get('update')->isClicked()){
+                $this->updateAmazonProduct($asin);
+            }
+        }
+        return $this->render('dashboard/admin_product.html.twig', [
+            'controller_name' => 'DashboardController',
+            'asinForm' => $form->createView(),
+        ]);
+    }
+
+
+    //Scrap product with asin to persist into db
+    public function scraperAmazonProduct($asin){
+
+        $ApiProduct = $this->AmazonApi->fetchAmazonProduct($asin);
+
+        $entityManager = $this->doctrine->getManager();
+
+        $product = new Product();
+        $product->setTitle($ApiProduct['product']['title']);
+
+        $product->setAsin($asin);
+        $product->setRating($ApiProduct['product']['rating']);
+        $product->setPrice($ApiProduct['product']['buybox_winner']['price']['value']);
+
+        $product->setAlink('empty');
+        if (isset($ApiProduct['product']['description'])){
+            $product->setDescription($ApiProduct['product']['description']);
+        }
+        $product->setLevel('best');
+        $product->setImage($ApiProduct['product']['main_image']['link']);
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+    }
+
+    //update product with asin to persist into db
+    public function updateAmazonProduct($asin){
+        $ApiProduct = $this->AmazonApi->fetchAmazonProduct($asin);
+        $entityManager = $this->doctrine->getManager();
+
+        $product = $this->doctrine->getRepository(Product::class)->findOneBy(['asin'=>$asin]);
+        $product->setTitle($ApiProduct['product']['title']);
+
+        $product->setAsin($asin);
+        $product->setRating($ApiProduct['product']['rating']);
+        $product->setPrice($ApiProduct['product']['buybox_winner']['price']['value']);
+
+        $product->setAlink('empty');
+        $product->setDescription($ApiProduct['product']['description']);
+        $product->setLevel('best');
+        $product->setImage($ApiProduct['product']['main_image']['link']);
+        $entityManager->persist($product);
+        $entityManager->flush();
+    }
+
 }
