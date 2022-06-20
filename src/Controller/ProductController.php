32f1bc099d9d760c1cfe5bfa9a51b485b6c6ab5e
Emrik <lecomtemrik@gmail.com>
2022-06-20 22:12:19 +0200

    Update ProductController (show by rank)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use App\Repository\RankRepository;
 use App\Service\AmazonApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,12 +28,66 @@ class ProductController extends AbstractController
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
-        foreach ($products as $product){
+        foreach ($products as $product) {
             dump($product->getSubcategory()->getTitle());
         }
 
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+            'products' => $products,
+        ]);
+
+    }
+
+
+    #[Route('/bestsellers', name: 'best_sellers')]
+    public function bestSellers(RankRepository $rankRepository): Response
+    {
+        $ranks = $rankRepository->findBy(['title'=>'meilleure vente']);
+        foreach ($ranks as $rank){
+            $products = $rank->getProducts();
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/bestprice', name: 'best_price')]
+    public function bestPrice(RankRepository $rankRepository): Response
+    {
+        $ranks = $rankRepository->findBy(['title'=>'meilleur prix']);
+        foreach ($ranks as $rank){
+            $products = $rank->getProducts();
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/bestquality', name: 'best_quality')]
+    public function bestQuality(RankRepository $rankRepository): Response
+    {
+        $ranks = $rankRepository->findBy(['title'=>'meilleure qualité']);
+        foreach ($ranks as $rank){
+            $products = $rank->getProducts();
+        }
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/pricequality', name: 'price_quality')]
+    public function priceQuality(RankRepository $rankRepository): Response
+    {
+        $ranks = $rankRepository->findBy(['title'=>'Rapport qualité/prix']);
+        foreach ($ranks as $rank){
+            $products = $rank->getProducts();
+        }
+
+        return $this->render('product/index.html.twig', [
             'products' => $products,
         ]);
     }
