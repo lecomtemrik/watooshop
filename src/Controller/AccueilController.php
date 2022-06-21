d293494fd447f159fa22045d5946ea0b68da33d5
Emrik <lecomtemrik@gmail.com>
2022-06-21 21:59:20 +0200

    set session for subcategory menu

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\SubCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(SubCategoryRepository $subCategoryRepository): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+        $session = new Session();
+        $session->clear();
+        $session->set('subCats', $subCategoryRepository->findAll());
+
+        return $this->render('base.html.twig');
     }
 }
